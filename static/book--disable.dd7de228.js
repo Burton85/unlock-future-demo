4fa4c9a818ebff9f12a49ecec79821e8a0ef6dc2
BurtonZH Hou <burtonzh.hou@ogilvy.com>
2023-12-05 00:31:18 +0800

    close menu

---
+++ static/book--disable.dd7de228.js
@@ -0,0 +1,1 @@
+import"./entry.cd45aa41.js";const o=""+globalThis.__publicAssetsURL("webp/podcast.webp"),p=""+globalThis.__publicAssetsURL("webp/book--disable.webp");export{o as _,p as a};
--- static/book--disable.eed95df2.js
+++
@@ -1,1 +0,0 @@
-import"./entry.a02c7620.js";const o=""+globalThis.__publicAssetsURL("webp/podcast.webp"),p=""+globalThis.__publicAssetsURL("webp/book--disable.webp");export{o as _,p as a};
