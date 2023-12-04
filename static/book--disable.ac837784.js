4692253b784916bc78b8eb62b9f17a7a6ed1e898
BurtonZH Hou <burtonzh.hou@ogilvy.com>
2023-12-04 22:06:37 +0800

    fix router

--- static/book--disable.ac837784.js
+++
@@ -1,1 +0,0 @@
-import"./entry.de6074ec.js";const o=""+globalThis.__publicAssetsURL("webp/podcast.webp"),p=""+globalThis.__publicAssetsURL("webp/book--disable.webp");export{o as _,p as a};
---
+++ static/book--disable.e37d7cf2.js
@@ -0,0 +1,1 @@
+import"./entry.9fed25a3.js";const o=""+globalThis.__publicAssetsURL("webp/podcast.webp"),p=""+globalThis.__publicAssetsURL("webp/book--disable.webp");export{o as _,p as a};
